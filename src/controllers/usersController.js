0a053c96a12c3edcfea6dcb4ee767c60ae5d537f
juan salazar <juancamilosalazarrestrepo@gmail.com>
2022-03-06 22:24:56 -0500

    validacion de completa

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const usersFilePath = path.join(__dirname, '../database/users.JSON');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const { validationResult } = require('express-validator');
+
 
 module.exports = {
     users:(req,res) =>{
@@ -19,26 +21,38 @@ module.exports = {
    },
 
    store: (req, res) => {
+
+    let errores = validationResult(req);
+    if (errores.isEmpty()){
+        if(req.file) {
+    
+
+            let newUser = {
+                id: users[users.length - 1].id + 1,
+                
+                ...req.body,
+                image: req.file.filename
+            };
+        
+            users.push(newUser);
+            fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+        
+            res.redirect('/users');
+    
+        }else{
+            res.render("register");
+        }
+    }else{
+        res.render("register",{
+            errors : errores.mapped(),
+            old: req.body
+         });
+        
+    }
+     
     console.log(req.body);
 
-    if(req.file) {
-    
-
-        let newUser = {
-            id: users[users.length - 1].id + 1,
-            
-            ...req.body,
-            image: req.file.filename
-        };
-    
-        users.push(newUser);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-    
-        res.redirect('/');
-
-    }else{
-        res.render("register");
-    }
+   
     
    
     
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,38 @@ var router = express.Router();
 const multer = require('multer');
 const path = require('path');
 
+// llamando a express validator
+
+const {body} = require ('express-validator');
+
+// validaciones
+const validacioneslogin = [
+  body('name').notEmpty().withMessage('Debes completar el campo de nombre'),
+  body('email').notEmpty().withMessage('debes Ingresar un correo').bail()
+  .isEmail().withMessage('debes escribir un formato de correo valido'),
+  body('password').notEmpty().withMessage('Debes ingresar una contraseña'),
+  body('age').notEmpty().withMessage('debes ingresar tu edad'),
+  body('country').notEmpty().withMessage('Debes escoje un pais'),
+  body('imagenUsuario').custom((value,{req}) => {
+    let file = req.file;
+    let acceptedExtensions = [ '.jpg','.png','.gif'];
+
+
+    if (!file) {
+      throw new Error('Tienes que subir una imagen')
+    }else {
+      let fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)){
+        throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(',')}`);
+      }
+
+    }
+    
+    return true;
+  })
+
+]
+
 const storage = multer.diskStorage({
   destination:(req,file,cb) => {
     cb(null,path.resolve(__dirname,'../../public/images'))
@@ -22,7 +54,7 @@ router.get('/', usersController.users);
 
 router.get('/register',usersController.register);
 
-router.post('/register',upload.single('imagenUsuario'), usersController.store);
+router.post('/register',upload.single('imagenUsuario'),validacioneslogin, usersController.store);
 
 
 
