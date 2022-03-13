fe451cab4463d028861525e5547832c600e67bcc
juan salazar <juancamilosalazarrestrepo@gmail.com>
2022-03-13 14:08:04 -0500

    login con validaciones session y cookies y hashing

---
+++ models/Users.js
@@ -0,0 +1,13 @@
+// guardar el usuario en la db
+//buscar al usuario que s equiere loguear por su email
+// buscar a un usuario por su id
+//editar la informacion de un usuario
+//eliminar a un usuario de la db
+
+//CRUD
+
+const User = {
+    create: function(userData){
+        
+    }
+}
